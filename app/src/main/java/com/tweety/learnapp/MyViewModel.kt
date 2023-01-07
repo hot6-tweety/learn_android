c2b55dc7a70105eb5293841444fead58b3af949e
kyj <flysoar3029@gmail.com>
2023-01-07 18:29:51 +0900

    [Add] DataBinding 의 양방향 데이터 바인딩

--- app/src/main/java/com/tweety/learnapp/MyViewModel.kt
+++ app/src/main/java/com/tweety/learnapp/MyViewModel.kt
@@ -8,12 +8,14 @@ import androidx.lifecycle.ViewModel
 
 class MyViewModel(_counter: Int, private val savedStateHandle: SavedStateHandle) : ViewModel() {
 //    var counter = _counter
+    var counter = savedStateHandle.get<Int>(SAVE_STATE_KEY) ?: _counter
+
     var liveCounter : MutableLiveData<Int> = MutableLiveData(_counter)
     val modifiedCounter : LiveData<String> = Transformations.map(liveCounter) { counter ->
         "LiveData 타입의 $counter 를 반환합니다"
     } // switchMap 함수는 매핑한 변수도 바꾼다...
-    var counter = savedStateHandle.get<Int>(SAVE_STATE_KEY) ?: _counter
 
+    val hasChecked : MutableLiveData<Boolean> = MutableLiveData<Boolean>(false)
     fun saveState() {
         savedStateHandle.set(SAVE_STATE_KEY, counter)
     }
